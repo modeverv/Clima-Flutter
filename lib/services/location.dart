e29527a527586011d9996c8beac86d31c7b4887a
seijiro <modeverv@gmail.com>
2022-01-31 13:05:22 +0900

    2022-01-31 13:05:22

---
+++ lib/services/location.dart
@@ -0,0 +1,46 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude = 0.0;
+  double longitude = 0.0;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        // Location services are not enabled don't continue
+        // accessing the position and request users of the
+        // App to enable the location services.
+        throw 'Location services are disabled.';
+      }
+
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          // Permissions are denied, next time you could try
+          // requesting permissions again (this is also where
+          // Android's shouldShowRequestPermissionRationale
+          // returned true. According to Android guidelines
+          // your App should show an explanatory UI now.
+          throw 'Location permissions are denied';
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        // Permissions are denied forever, handle appropriately.
+        throw 'Location permissions are permanently denied, we cannot request permissions.';
+      }
+
+      // When we reach here, permissions are granted and we can
+      // continue accessing the position of the device.
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      print(position);
+      this.longitude = position.longitude;
+      this.latitude = position.latitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
